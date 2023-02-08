378184c7174dd09a8974e2deca1c023499106b48
sy-122 <sbak266@gmail.com>
2023-02-08 11:43:17 +0000

    Improved python code

--- comp0034-cw1/dash_app.py
+++ comp0034-cw1/dash_app.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output, State, MATCH
 import dash_daq as daq
 
 # ------------------------#
-# Create Graphs
+# Import Data and Prepare
 # ------------------------#
 # Import Data
 BIKE_DATA_FILEPATH = Path(__file__).parent.joinpath('data', 'Bike_data_adjusted.csv')
@@ -16,53 +16,59 @@ df_bike = pd.read_csv(BIKE_DATA_FILEPATH, encoding='unicode_escape')
 # Convert 'Count' to integer
 df_bike['Count'] = df_bike['Count'].astype(int)
 
+# Dataframe for percent of holiday in each month (for bar graph)
+bar_original_df = df_bike.groupby(['Month']).mean('Count').reset_index()
+Month_Holiday = df_bike.groupby(['Month'])['Holiday'].\
+    value_counts(normalize=True).reset_index(name='Holiday%')
+Month_Holiday = Month_Holiday[Month_Holiday.Holiday == 'Holiday']
+Month_Holiday['Holiday%'] = Month_Holiday['Holiday%'].apply(lambda x: x * 100)
+Month_Holiday['Holiday%'] = Month_Holiday['Holiday%'].apply(lambda x: '{0:1.2f}%'.format(x))
+bar_new_df = pd.merge(bar_original_df, Month_Holiday, how='left', on='Month')
+
+# ------------------------#
+# Create graphs
+# ------------------------#
 # Line plot for Hour VS Bicycle Rented
 hour_line_plot1 = px.line(
     data_frame=df_bike.groupby(['DayofWeek', 'Hour']).mean('Count').reset_index(),
     x='Hour',
     y='Count',
     category_orders={'DayofWeek': ['Sunday', 'Monday', 'Tuesday',
-                                   'Wednesday', 'Thurday', 'Friday', 'Saturday']},
+                                   'Wednesday', 'Thurday', 'Friday',
+                                   'Saturday']},
     color='DayofWeek',
     labels={'Hour': 'Time', 'Count': 'Average Bike Rented'},
     markers=True,
     template='simple_white'
 )
 
-# Dataframe needed for the bar graph
-bar_original_df = df_bike.groupby(['Month']).mean('Count').reset_index()
-Month_Holiday = df_bike.groupby(['Month'])['Holiday'].value_counts(
-    normalize=True).reset_index(name='Holiday%')
-Month_Holiday = Month_Holiday[Month_Holiday.Holiday == 'Holiday']
-Month_Holiday['Holiday%'] = Month_Holiday['Holiday%'].apply(lambda x: x * 100)
-Month_Holiday['Holiday%'] = Month_Holiday['Holiday%'].apply(lambda x: '{0:1.2f}%'.format(x))
-bar_new_df = pd.merge(bar_original_df, Month_Holiday, how='left', on='Month')
-
-# Bar graph for Month vs Bicycle Rented
+# Make bar graph for Month vs Bicycle Rented
 def month_bar_graph(df=bar_original_df, bar_text=None):
     fig = px.bar(
         data_frame=df,
         x='Month',
-        y="Count",
+        y='Count',
         text=bar_text,
-        labels={'Month': 'Month', 'Count': 'Average Bike Rented per hour'},
-        template="simple_white")
+        labels={'Month': 'Month',
+                'Count': 'Average Bike Rented per hour'},
+        template='simple_white'
+    )
     return fig
 
-# Make Scatter Plot with Y-VAR as total number of bike rented
+# Make Scatter Plot with different x-variables
 def scatter_plot(x_var):
     fig = px.scatter(
         data_frame=df_bike,
         x=x_var,
-        y="Count",
+        y='Count',
         trendline='ols',
-        trendline_color_override="red",
+        trendline_color_override='red',
         opacity=0.3,
-        labels={'x': x_var, 'Count': 'Bike Rented'},
-        template="simple_white"
+        labels={'x': x_var,
+                'Count': 'Bike Rented'},
+        template='simple_white'
     )
     return fig
-
 
 # ------------------------#
 # Create App
@@ -80,16 +86,16 @@ navbar = dbc.NavbarSimple(
     children=[
         dbc.DropdownMenu(
             children=[
-                dbc.DropdownMenuItem("Home", href="/", active="exact"),
-                dbc.DropdownMenuItem("Comments", href="/", active="exact")
+                dbc.DropdownMenuItem('Home', href='/', active='exact'),
+                dbc.DropdownMenuItem('Comments', href='/', active='exact')
             ],
             nav=True,
             in_navbar=True,
-            label="Menu",
+            label='Menu',
         ),
     ],
-    brand="Seoul Bicycle",
-    brand_href="#",
+    brand='Seoul Bicycle',
+    brand_href='#',
     color='secondary',
     className='nav-tab'
 )
@@ -99,23 +105,21 @@ app.layout = dbc.Container(
     children=[
         navbar,
         html.H1(children='Welcome to Seoul Public Bicycle Website!',
-                className="text-center p-4"),
+                className='text-center p-4'),
         html.Img(src=app.get_asset_url('seoul-bike.png'),
                  className='center'),
         html.P(children='Learn about the different variables that affect the usage of bike.',
-               className="text-center p-2"),
+               className='text-center p-2'),
         html.Br(),
-
-        dcc.Tabs(id="nav-tabs", value='tab-content-graph', children=[
+        dcc.Tabs(id='nav-tabs', value='tab-content-graph',
+                 children=[
             dcc.Tab(label='Time & Month', value='time-related'),
             dcc.Tab(label='Other Variables', value='others'),
         ]),
         html.Div(id='tabs-content-graph')
-
     ],
     fluid=True
 )
-
 
 # Callback for Tab
 @app.callback(Output(component_id='tabs-content-graph', component_property='children'),
@@ -136,7 +140,7 @@ def render_content(tab):
                     id='boolean-switch',
                     on=False,
                     color='#78c2ad',
-                    label="% of days that are Holiday"
+                    label='% of days that are Holiday'
                 ),
                 html.Div(
                     [dcc.Graph(id='bar')], id='boolean-switch-output')
@@ -209,7 +213,7 @@ def display_graphs(n_clicks, div_children):
     Input(component_id={'type': 'scatter-dropdown', 'index': MATCH}, component_property='value')
 )
 def update_scatter_plot(x_variable):
-    # if not selected return blank graph
+    # if no x-variable selected return blank graph
     if x_variable is None:
         return {'data': []}
     else:
