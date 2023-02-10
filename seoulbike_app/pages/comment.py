ec0da188f10b3b7f69432a21d1abafff6ca66fa9
sy-122 <sbak266@gmail.com>
2023-02-10 20:29:34 +0000

    Comment page updated so previous comment can be seen

--- seoulbike_app/pages/comment.py
+++ seoulbike_app/pages/comment.py
@@ -4,23 +4,31 @@ from dash.dependencies import Input, Output, State
 
 dash.register_page(__name__)
 
+# Comment page layout using Bootstrap fluid container
 layout = html.Div([
     html.H3(children='Write Comments'),
+    html.P(children='Please share your thoughts about the app so we can improve.'),
     dcc.Textarea(
-        id='textarea',
+        id='comment-area',
         value='',
         style={'width': '100%', 'height': 200},
     ),
-    html.Button('Submit', id='submit-button', n_clicks=0),
+    html.Button('Submit', id='button', n_clicks=0),
     html.Div(id='comment-output', style={'whiteSpace': 'pre-line'})
 ])
 
 
+# Submit comment when pushed 'Submit' and record all comments submitted
 @callback(
     Output('comment-output', 'children'),
-    Input('submit-button', 'n_clicks'),
-    State('textarea', 'value')
+    Input('button', 'n_clicks'),
+    [
+        State('comment-area', 'value'),
+        State('comment-output', 'children'),
+    ],
 )
-def update_output(n_clicks, value):
+def update_output(n_clicks, value, children):
     if n_clicks > 0:
-        return 'Submitted Comment: {}'.format(value)
+        return f'{children}\n Submitted Comment: {value}'
+    else:
+        return ''
